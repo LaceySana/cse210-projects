5cf33146c780e480e272c63cafc341af812cda0f
LaceySana <129418829+LaceySana@users.noreply.github.com>
2025-09-13 12:14:48 -0600

    added menu and most program functions

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,3 +1,5 @@
+using System.IO;
+
 public class Journal
 {
     public List<Entry> _entries = new List<Entry>();
@@ -17,6 +19,27 @@ public class Journal
 
     public void SaveJournal(string filename)
     {
-        
+        Console.WriteLine("Saving to file...");
+
+        using (StreamWriter outputFile = new StreamWriter(filename))
+        {
+            foreach (Entry e in _entries)
+            {
+                outputFile.WriteLine($"{e._dateText}~|~{e._promptText}~|~{e._responseText}");
+            }
+        }
+    }
+
+    public void LoadJournal(string filename)
+    {
+        Console.WriteLine("Loading file...");
+
+        string[] lines = System.IO.File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("~");
+
+        }
     }
 }
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,71 @@
 using System;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+
+        Journal currentJournal = new Journal();
+
+        string menuInput = null;
+        do
+        {
+            Console.WriteLine("""
+
+                Daily Journal
+                1. New Entry
+                2. Display Journal
+                3. Save Journal
+                4. Load Journal
+                5. Quit
+            """);
+            Console.Write(" >> ");
+            menuInput = Console.ReadLine();
+
+            if (menuInput == "1")
+            {
+                DateTime currentTime = DateTime.Now;
+                string dateText = currentTime.ToShortDateString();
+                PromptGenerator promptGen = new PromptGenerator();
+                string randPrompt = promptGen.GetRandomPrompt();
+
+                Console.WriteLine($"\n{string.Concat(Enumerable.Repeat('*', 100))}\n{dateText}\n{randPrompt}");
+                Console.Write("\n > ");
+                string responseInput = Console.ReadLine();
+
+                Entry newEntry = new Entry();
+                newEntry._dateText = dateText;
+                newEntry._promptText = randPrompt;
+                newEntry._responseText = responseInput;
+
+                currentJournal.AddEntry(newEntry);
+            }
+            else if (menuInput == "2")
+            {
+                currentJournal.DisplayJournal();
+            }
+            else if (menuInput == "3")
+            {
+                Console.Write("What file name would you like to save this journal as?\n >> ");
+                string saveAs = Console.ReadLine();
+                currentJournal.SaveJournal(saveAs);
+            }
+            else if (menuInput == "4")
+            {
+                Console.Write("Which file would you like to load from?\n >> ");
+                string loadFrom = Console.ReadLine();
+                currentJournal.LoadJournal(loadFrom);
+            }
+            else if (menuInput == "5")
+            {
+                continue;
+            }
+            else
+            {
+                Console.WriteLine("-Invalid Option-");
+            }
+        } while (menuInput != "5");
     }
 }
